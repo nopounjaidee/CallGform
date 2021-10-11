b9cd4f03b800c8abbfaed8ab521b647fd31f5e4b
นพ อุ่นใจดี <looklink99@hotmail.com>
2021-10-11 16:01:54 +0700

    M002

--- app.js
+++ app.js
@@ -58,9 +58,9 @@ var TypeName = async (link) => {
 async function CallStart(){
   // Param 1  ทะเบียน
   // Param 2  ลิงค์ = MTPR : KBLC : KSLC : KMLC : KPLC : TEST
-  // Calling("3ฒญ5043",Dic.MTPR); 
-  Calling("3ฒญ5043",Dic.KBLC); 
-  Calling("3ฒญ5043",Dic.KSLC); 
+  Calling("3ฒญ5043",Dic.MTPR); 
+  // Calling("3ฒญ5043",Dic.KBLC); 
+  // Calling("3ฒญ5043",Dic.KSLC); 
   // Calling("Oicoru 401",Dic.TESTA); 
   // console.log("lamm")
 }
