ed4da81579e76a46de6f0d1ba9d705a934ab6e33
นพ อุ่นใจดี <looklink99@hotmail.com>
2021-10-22 16:07:19 +0700

    bbss

--- app.js
+++ app.js
@@ -57,15 +57,15 @@ var TypeName = async (link) => {
 async function CallStart(){
   // Param 1  ทะเบียน
   // Param 2  ลิงค์ = MTPR : KBLC : KSLC : KMLC : KPLC : TEST
-  //   Calling("3ฒฉ1862",Dic.MTPR); 
-//   Calling("3ฒญ5043",Dic.MTPR); 
-//   Calling("ข0624",Dic.MTPR); 
-  Calling("3ฒฉ1862",Dic.KBLC); 
-  Calling("3ฒญ5043",Dic.KBLC); 
-  Calling("ข0624",Dic.KBLC); 
-  Calling("3ฒฉ1862",Dic.KSLC); 
-  Calling("3ฒญ5043",Dic.KSLC); 
-  Calling("ข0624",Dic.KSLC); 
+    Calling("3ฒฉ1862",Dic.MTPR); 
+  Calling("3ฒญ5043",Dic.MTPR); 
+  Calling("ข0624",Dic.MTPR); 
+  // Calling("3ฒฉ1862",Dic.KBLC); 
+  // Calling("3ฒญ5043",Dic.KBLC); 
+  // Calling("ข0624",Dic.KBLC); 
+  // Calling("3ฒฉ1862",Dic.KSLC); 
+  // Calling("3ฒญ5043",Dic.KSLC); 
+  // Calling("ข0624",Dic.KSLC); 
 }
 async function Retry(CarReg,link){
   Calling(CarReg,link);
