15c9d6ea9404a654f1ddf177c51ffb25e48742b8
นพ อุ่นใจดี <looklink99@hotmail.com>
2021-11-29 15:40:51 +0700

    lam

--- app.js
+++ app.js
@@ -136,9 +136,18 @@ async function callpost(Gform,person){
 async function CallStart(){
   // Param 1  ลิงค์ = MTPR : KBLC : KSLC : KMLC : KPLC : TESTA : TESTB : TESTC
   // Param 2  ID = 1 : "บุญมา" | ID = 2 : "วีรศักดิ์" | ID = 3 : "ตุลาพร สีจุ้ย" | ID = 4 : "ณัฐสิทธิ์ อ่วมสอาด"  | ID = 5 : "นนทชัย แสนศรี"  | ID = 6 : "เกียรติพิทักษ์  แน่นอุดร"
-  Calling("KBLC","7")
-  Calling("KSLC","7")
-  Calling("KMLC","7")
+  Calling("KBLC","1")
+  Calling("KSLC","1")
+  Calling("KMLC","1")
+
+  Calling("KBLC","2")
+  Calling("KSLC","2")
+  Calling("KMLC","2")
+
+  Calling("KBLC","3")
+  Calling("KSLC","3")
+  Calling("KMLC","3")
+  
   // Calling("MTPR","7")
 }
 async function Retry(forms,person){
