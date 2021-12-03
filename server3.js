49c155ceafac97aec44d8a48fb8743c212790e3b
นพ อุ่นใจดี <looklink99@hotmail.com>
2021-12-03 16:01:17 +0700

    sas

--- server3.js
+++ server3.js
@@ -195,7 +195,7 @@ async function callpost(Gform,person){
 });
 }
  GetdataSheet()
-const job = schedule.scheduleJob('1 16 * * *', function(){
+const job = schedule.scheduleJob('2 16 * * *', function(){
   console.log('Start..................');
   CallStart()
 });
